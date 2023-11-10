5e029bc9bf76f145c5991950c326aa51296a0d02
BaselHosari88 <bassel.housri88@gmail.com>
2023-11-10 09:45:34 +0100

    adding new controller making show and index from Crud

---
+++ baselt/resources/views/listings.blade.php
@@ -0,0 +1,64 @@
+
+
+@extends('layout')
+@section('content')
+
+@foreach ($listings as $listing)
+
+
+<a href="/listings/{{$listing['id']}}">ID </a>
+
+
+
+    <ul class="list-group">
+        <h5>Title is </h1>
+        <li class="list-group-item"> {{$listing['title']}}</li> <br>
+
+        <h5>tags is </h5>
+        <li class="list-group-item"> {{$listing['tags']}}</li> <br>
+
+        <h5>company is </h5>
+        <li class="list-group-item">{{$listing['company']}}</li><br>
+
+        <h5>location is</h5>
+        <li class="list-group-item"> {{$listing['location']}}</li><br>
+
+
+
+  </ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+
+@endforeach
+
+
+@endsection
+
+
+
+
+
+
+
+
+
+
